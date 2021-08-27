55ce90d4af57f34a3a9def5b11eb7be9590e452a
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-08-27 10:31:11 +0200

    paths to img/....png in main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,9 +8,9 @@ export class MainView extends React.Component {   // here you export the MainVie
     super();
     this.state = {  //MainView's state has objects --> movies, selectedMovie
       movies: [
-        { _id: 1, title: 'Inception', description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', imageUrl: 'https://www.imdb.com/title/tt1375666/mediaviewer/rm3426651392/?ref_=tt_ov_i'},
-        { _id: 2, title: 'The Shawshank Redemption', description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', imageUrl: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm10105600/?ref_=tt_ov_i'},
-        { _id: 3, title: 'Gladiator', description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', imageUrl: 'https://www.imdb.com/title/tt0172495/mediaviewer/rm2442542592/?ref_=tt_ov_i'}
+        { _id: 1, title: 'Inception', description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', imageUrl: 'C:/Users/ASUS/Documents/careerfoundry/myFlix-client/img/Inception.png'},
+        { _id: 2, title: 'The Shawshank Redemption', description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', imageUrl: 'C:/Users/ASUS/Documents/careerfoundry/myFlix-client/img/TheShawshankRedemption.png'},
+        { _id: 3, title: 'Gladiator', description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', imageUrl: 'C:/Users/ASUS/Documents/careerfoundry/myFlix-client/img/Gladiator.png'}
       ],
       selectedMovie: null
     };
