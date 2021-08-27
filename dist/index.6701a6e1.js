e80b29e743d830644af78f8b9f81e96aa27db3f5
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-08-27 11:28:35 +0200

    changing paths to pictures

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -22865,6 +22865,7 @@ var _react = require("react");
 var _reactDefault = parcelHelpers.interopDefault(_react);
 var _movieCard = require("../movie-card/movie-card");
 var _movieView = require("../movie-view/movie-view");
+var _cors = require("cors");
 class MainView extends _reactDefault.default.Component {
     constructor(){
         super();
@@ -22880,13 +22881,13 @@ class MainView extends _reactDefault.default.Component {
                     _id: 2,
                     title: 'The Shawshank Redemption',
                     description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-                    imageUrl: 'https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg'
+                    imageUrl: 'C:/Users/ASUS/Documents/careerfoundry/myFlix-client/img/TheShawshankRedemption.jpg'
                 },
                 {
                     _id: 3,
                     title: 'Gladiator',
                     description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.',
-                    imageUrl: 'https://upload.wikimedia.org/wikipedia/en/f/fb/Gladiator_%282000_film_poster%29.png'
+                    imageUrl: '../../img/Gladiator.jpg'
                 }
             ],
             selectedMovie: null
@@ -22906,7 +22907,7 @@ class MainView extends _reactDefault.default.Component {
             className: "main-view",
             __source: {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 32
+                lineNumber: 33
             },
             __self: this,
             children: "The list is empty!"
@@ -22915,7 +22916,7 @@ class MainView extends _reactDefault.default.Component {
             className: "main-view",
             __source: {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 35
+                lineNumber: 36
             },
             __self: this,
             children: selectedMovie ? /*#__PURE__*/ _jsxRuntime.jsx(_movieView.MovieView, {
@@ -22925,7 +22926,7 @@ class MainView extends _reactDefault.default.Component {
                 },
                 __source: {
                     fileName: "src/components/main-view/main-view.jsx",
-                    lineNumber: 37
+                    lineNumber: 38
                 },
                 __self: this
             }) : movies.map((movie)=>/*#__PURE__*/ _jsxRuntime.jsx(_movieCard.MovieCard, {
@@ -22935,7 +22936,7 @@ class MainView extends _reactDefault.default.Component {
                     },
                     __source: {
                         fileName: "src/components/main-view/main-view.jsx",
-                        lineNumber: 39
+                        lineNumber: 40
                     },
                     __self: this
                 }, movie._id)
@@ -22949,7 +22950,7 @@ class MainView extends _reactDefault.default.Component {
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-runtime":"8xIwr","react":"6TuXu","../movie-card/movie-card":"6EiBJ","../movie-view/movie-view":"ikZdr","@parcel/transformer-js/src/esmodule-helpers.js":"2govV","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"3tkSd"}],"6EiBJ":[function(require,module,exports) {
+},{"react/jsx-runtime":"8xIwr","react":"6TuXu","../movie-card/movie-card":"6EiBJ","../movie-view/movie-view":"ikZdr","@parcel/transformer-js/src/esmodule-helpers.js":"2govV","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"3tkSd","cors":"5mCT9"}],"6EiBJ":[function(require,module,exports) {
 var $parcel$ReactRefreshHelpers$4249 = require("@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js");
 var prevRefreshReg = window.$RefreshReg$;
 var prevRefreshSig = window.$RefreshSig$;
@@ -23136,6 +23137,291 @@ class MovieView extends _reactDefault.default.Component {
   window.$RefreshReg$ = prevRefreshReg;
   window.$RefreshSig$ = prevRefreshSig;
 }
-},{"react/jsx-runtime":"8xIwr","react":"6TuXu","@parcel/transformer-js/src/esmodule-helpers.js":"2govV","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"3tkSd"}]},["drWAM","l0xXt","dLPEP"], "dLPEP", "parcelRequireaec4")
+},{"react/jsx-runtime":"8xIwr","react":"6TuXu","@parcel/transformer-js/src/esmodule-helpers.js":"2govV","@parcel/transformer-react-refresh-wrap/lib/helpers/helpers.js":"3tkSd"}],"5mCT9":[function(require,module,exports) {
+(function() {
+    var assign = require('object-assign');
+    var vary = require('vary');
+    var defaults = {
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+        optionsSuccessStatus: 204
+    };
+    function isString(s) {
+        return typeof s === 'string' || s instanceof String;
+    }
+    function isOriginAllowed(origin, allowedOrigin) {
+        if (Array.isArray(allowedOrigin)) {
+            for(var i = 0; i < allowedOrigin.length; ++i){
+                if (isOriginAllowed(origin, allowedOrigin[i])) return true;
+            }
+            return false;
+        } else if (isString(allowedOrigin)) return origin === allowedOrigin;
+        else if (allowedOrigin instanceof RegExp) return allowedOrigin.test(origin);
+        else return !!allowedOrigin;
+    }
+    function configureOrigin(options, req) {
+        var requestOrigin = req.headers.origin, headers = [], isAllowed;
+        if (!options.origin || options.origin === '*') // allow any origin
+        headers.push([
+            {
+                key: 'Access-Control-Allow-Origin',
+                value: '*'
+            }
+        ]);
+        else if (isString(options.origin)) {
+            // fixed origin
+            headers.push([
+                {
+                    key: 'Access-Control-Allow-Origin',
+                    value: options.origin
+                }
+            ]);
+            headers.push([
+                {
+                    key: 'Vary',
+                    value: 'Origin'
+                }
+            ]);
+        } else {
+            isAllowed = isOriginAllowed(requestOrigin, options.origin);
+            // reflect origin
+            headers.push([
+                {
+                    key: 'Access-Control-Allow-Origin',
+                    value: isAllowed ? requestOrigin : false
+                }
+            ]);
+            headers.push([
+                {
+                    key: 'Vary',
+                    value: 'Origin'
+                }
+            ]);
+        }
+        return headers;
+    }
+    function configureMethods(options) {
+        var methods = options.methods;
+        if (methods.join) methods = options.methods.join(','); // .methods is an array, so turn it into a string
+        return {
+            key: 'Access-Control-Allow-Methods',
+            value: methods
+        };
+    }
+    function configureCredentials(options) {
+        if (options.credentials === true) return {
+            key: 'Access-Control-Allow-Credentials',
+            value: 'true'
+        };
+        return null;
+    }
+    function configureAllowedHeaders(options, req) {
+        var allowedHeaders = options.allowedHeaders || options.headers;
+        var headers = [];
+        if (!allowedHeaders) {
+            allowedHeaders = req.headers['access-control-request-headers']; // .headers wasn't specified, so reflect the request headers
+            headers.push([
+                {
+                    key: 'Vary',
+                    value: 'Access-Control-Request-Headers'
+                }
+            ]);
+        } else if (allowedHeaders.join) allowedHeaders = allowedHeaders.join(','); // .headers is an array, so turn it into a string
+        if (allowedHeaders && allowedHeaders.length) headers.push([
+            {
+                key: 'Access-Control-Allow-Headers',
+                value: allowedHeaders
+            }
+        ]);
+        return headers;
+    }
+    function configureExposedHeaders(options) {
+        var headers = options.exposedHeaders;
+        if (!headers) return null;
+        else if (headers.join) headers = headers.join(','); // .headers is an array, so turn it into a string
+        if (headers && headers.length) return {
+            key: 'Access-Control-Expose-Headers',
+            value: headers
+        };
+        return null;
+    }
+    function configureMaxAge(options) {
+        var maxAge = (typeof options.maxAge === 'number' || options.maxAge) && options.maxAge.toString();
+        if (maxAge && maxAge.length) return {
+            key: 'Access-Control-Max-Age',
+            value: maxAge
+        };
+        return null;
+    }
+    function applyHeaders(headers, res) {
+        for(var i = 0, n = headers.length; i < n; i++){
+            var header = headers[i];
+            if (header) {
+                if (Array.isArray(header)) applyHeaders(header, res);
+                else if (header.key === 'Vary' && header.value) vary(res, header.value);
+                else if (header.value) res.setHeader(header.key, header.value);
+            }
+        }
+    }
+    function cors(options, req, res, next) {
+        var headers = [], method = req.method && req.method.toUpperCase && req.method.toUpperCase();
+        if (method === 'OPTIONS') {
+            // preflight
+            headers.push(configureOrigin(options, req));
+            headers.push(configureCredentials(options, req));
+            headers.push(configureMethods(options, req));
+            headers.push(configureAllowedHeaders(options, req));
+            headers.push(configureMaxAge(options, req));
+            headers.push(configureExposedHeaders(options, req));
+            applyHeaders(headers, res);
+            if (options.preflightContinue) next();
+            else {
+                // Safari (and potentially other browsers) need content-length 0,
+                //   for 204 or they just hang waiting for a body
+                res.statusCode = options.optionsSuccessStatus;
+                res.setHeader('Content-Length', '0');
+                res.end();
+            }
+        } else {
+            // actual response
+            headers.push(configureOrigin(options, req));
+            headers.push(configureCredentials(options, req));
+            headers.push(configureExposedHeaders(options, req));
+            applyHeaders(headers, res);
+            next();
+        }
+    }
+    function middlewareWrapper(o) {
+        // if options are static (either via defaults or custom options passed in), wrap in a function
+        var optionsCallback = null;
+        if (typeof o === 'function') optionsCallback = o;
+        else optionsCallback = function(req, cb) {
+            cb(null, o);
+        };
+        return function corsMiddleware(req, res, next) {
+            optionsCallback(req, function(err, options) {
+                if (err) next(err);
+                else {
+                    var corsOptions = assign({
+                    }, defaults, options);
+                    var originCallback = null;
+                    if (corsOptions.origin && typeof corsOptions.origin === 'function') originCallback = corsOptions.origin;
+                    else if (corsOptions.origin) originCallback = function(origin, cb) {
+                        cb(null, corsOptions.origin);
+                    };
+                    if (originCallback) originCallback(req.headers.origin, function(err2, origin) {
+                        if (err2 || !origin) next(err2);
+                        else {
+                            corsOptions.origin = origin;
+                            cors(corsOptions, req, res, next);
+                        }
+                    });
+                    else next();
+                }
+            });
+        };
+    }
+    // can pass either an options hash, an options delegate, or nothing
+    module.exports = middlewareWrapper;
+})();
+
+},{"object-assign":"jzTFF","vary":"eTS1p"}],"eTS1p":[function(require,module,exports) {
+/*!
+ * vary
+ * Copyright(c) 2014-2017 Douglas Christopher Wilson
+ * MIT Licensed
+ */ 'use strict';
+/**
+ * Module exports.
+ */ module.exports = vary;
+module.exports.append = append;
+/**
+ * RegExp to match field-name in RFC 7230 sec 3.2
+ *
+ * field-name    = token
+ * token         = 1*tchar
+ * tchar         = "!" / "#" / "$" / "%" / "&" / "'" / "*"
+ *               / "+" / "-" / "." / "^" / "_" / "`" / "|" / "~"
+ *               / DIGIT / ALPHA
+ *               ; any VCHAR, except delimiters
+ */ var FIELD_NAME_REGEXP = /^[!#$%&'*+\-.^_`|~0-9A-Za-z]+$/;
+/**
+ * Append a field to a vary header.
+ *
+ * @param {String} header
+ * @param {String|Array} field
+ * @return {String}
+ * @public
+ */ function append(header, field) {
+    if (typeof header !== 'string') throw new TypeError('header argument is required');
+    if (!field) throw new TypeError('field argument is required');
+    // get fields array
+    var fields = !Array.isArray(field) ? parse(String(field)) : field;
+    // assert on invalid field names
+    for(var j = 0; j < fields.length; j++){
+        if (!FIELD_NAME_REGEXP.test(fields[j])) throw new TypeError('field argument contains an invalid header name');
+    }
+    // existing, unspecified vary
+    if (header === '*') return header;
+    // enumerate current values
+    var val = header;
+    var vals = parse(header.toLowerCase());
+    // unspecified vary
+    if (fields.indexOf('*') !== -1 || vals.indexOf('*') !== -1) return '*';
+    for(var i = 0; i < fields.length; i++){
+        var fld = fields[i].toLowerCase();
+        // append value (case-preserving)
+        if (vals.indexOf(fld) === -1) {
+            vals.push(fld);
+            val = val ? val + ', ' + fields[i] : fields[i];
+        }
+    }
+    return val;
+}
+/**
+ * Parse a vary header into an array.
+ *
+ * @param {String} header
+ * @return {Array}
+ * @private
+ */ function parse(header) {
+    var end = 0;
+    var list = [];
+    var start = 0;
+    // gather tokens
+    for(var i = 0, len = header.length; i < len; i++)switch(header.charCodeAt(i)){
+        case 32:
+            /*   */ if (start === end) start = end = i + 1;
+            break;
+        case 44:
+            /* , */ list.push(header.substring(start, end));
+            start = end = i + 1;
+            break;
+        default:
+            end = i + 1;
+            break;
+    }
+    // final token
+    list.push(header.substring(start, end));
+    return list;
+}
+/**
+ * Mark that a request is varied on a header field.
+ *
+ * @param {Object} res
+ * @param {String|Array} field
+ * @public
+ */ function vary(res, field) {
+    if (!res || !res.getHeader || !res.setHeader) // quack quack
+    throw new TypeError('res argument is required');
+    // get existing header
+    var val = res.getHeader('Vary') || '';
+    var header = Array.isArray(val) ? val.join(', ') : String(val);
+    // set new header
+    if (val = append(header, field)) res.setHeader('Vary', val);
+}
+
+},{}]},["drWAM","l0xXt","dLPEP"], "dLPEP", "parcelRequireaec4")
 
 //# sourceMappingURL=index.6701a6e1.js.map
