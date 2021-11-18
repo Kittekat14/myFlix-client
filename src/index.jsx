595e71cdf9788b1c0ee73e5010c78091387c6a29
Katrin Hofstetter <katrin_hofstetter@gmx.de>
2021-11-18 12:13:07 +0100

    deletion of store, provider and redux stuff

--- src/index.jsx
+++ src/index.jsx
@@ -1,35 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import MainView from './App';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import moviesApp from './reducers/reducers';
-import { devToolsEnhancer } from 'redux-devtools-extension';
+import MainView from './components/main-view';
 import Container from 'react-bootstrap/Container'
-
 // Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
 
-//creating my "store" = "globalized state"
-
-const store = createStore(moviesApp, devToolsEnhancer());
 
 // Main component (will eventually use all the others)
 class myFlixApplication extends React.Component {
   render() {
     return (
-      <Provider store={store}>
         <Container>
           <MainView />    
         </Container>
-      </Provider>
     );
   }
 }
 
 // Finds the root of your app
 const container = document.getElementsByClassName('app-container')[0];
-
 
 // Tells React to render your app in the root DOM element
 ReactDOM.render(React.createElement(myFlixApplication), container);
